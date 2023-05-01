f26a6805b6d591f666f5dfcc488b06eb1d793c24
Jay <j.adeyina@gmail.com>
2023-05-01 22:17:17 +0900

    Added CTA button

--- js/script.js
+++ js/script.js
@@ -4,7 +4,9 @@ const bars = document.querySelector(".fa-bars");
 bars.addEventListener("click", function() {
     if (overlay.classList.contains("hide")) {
         overlay.classList.remove("hide");
+        bars.style.opacity = "0.5";
     } else {
         overlay.classList.add("hide");
+        bars.style.opacity = "1";
     }
 });
